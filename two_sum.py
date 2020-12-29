2fc76fbca2dc38a1c421e6411c5b656aa54ca457
Alvin Ngo <aln011@ucsd.edu>
2020-12-28 19:02:49 -0800

    Added test cases for two sum

---
+++ two_sum.py
@@ -0,0 +1,22 @@
+def two_sum(nums, target):
+	pass
+
+nums1 = [2, 7, 11, 15] 
+target1 = 9
+print(two_sum(nums1, target1))
+#[2, 7]
+
+nums2 = [3, 2, 4] 
+target2 = 6
+print(two_sum(nums2, target2))
+#[2, 4]
+
+nums3 = [3, 3] 
+target3 = 6
+print(two_sum(nums3, target3))
+#[3, 3]
+
+nums4 = [3, 5, -4, 8, 11, 1, -1, 6] 
+target4 = 10 
+print(two_sum(nums4, target4))
+#[-1, 11] 
