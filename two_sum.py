aa9df8d6d9d068e556f8841dc034020e72a33dc9
Alvin Ngo <aln011@ucsd.edu>
2020-12-29 22:34:13 -0800

    Finished adding in the code for two sum

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,55 @@
+#Runtime: O(nlogn) | Space: O(1)
+#         where n is the number of vals in list
 def two_sum(nums, target):
-	pass
+	#First sort the list in ascending order
+	#Have 1 ptr at the beginning of the list (smallest num) & 1 ptr at the end of the list (largest num)
+	#While the left ptr is less than the right ptr, continue loop 
+		#At each loop, first check to see if the two values sum to the target
+		#If they do, return the two values
+		#If they dont, check to see if the sum is less than or greater than the target
+			#If greater, decrement the right ptr(move to left)
+			#If less than, increment the left ptr(move to right)
+	nums.sort()
+	leftPtr = 0
+	rightPtr = len(nums) - 1
+	while leftPtr < rightPtr:
+		currSum = nums[leftPtr] + nums[rightPtr]
+		if currSum == target:
+			return [nums[leftPtr], nums[rightPtr]]
+		elif currSum > target:
+			rightPtr -= 1
+		else:
+			leftPtr += 1
+	return []
+
+
+#Runtime: O(n) | Space: O(n)
+#         where n is the number of vals in list 
+def two_sum2(nums, target):
+	#Go through each num in list and gets its difference from the target value
+	#Check to see if the difference has been seen before by checking the "seen" dictionary
+	#If it has, then return the [difference, current num]. Otherwise, add the current num to the dictionary
+	seenDict = {}
+	for num in nums:
+		difference = target - num
+		if difference in seenDict:
+			return [difference, num]
+		else:
+			seenDict[num] = True
+	return []
+
+
+#Runtime: O(n^2) | Space: O(1)
+#         where n is the number of vals in list
+def two_sum3(nums, target):
+	#Go through every possible pair of numbers to find the target
+	for i in range(len(nums)-1):
+		for j in range(i+1, len(nums)):
+			currSum = nums[i] + nums[j]	
+			if currSum == target:
+				return [nums[i], nums[j]]
+	return []
+
 
 nums1 = [2, 7, 11, 15] 
 target1 = 9
