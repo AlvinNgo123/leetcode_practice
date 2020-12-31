263ff63694ce4407ba86ad061519b36bb1ab668b
Alvin Ngo <aln011@ucsd.edu>
2020-12-31 11:21:30 -0800

    Finished writing and testing code for is_subsequence

--- is_subsequence.py
+++ is_subsequence.py
@@ -1,5 +1,30 @@
+#Runtime: O(n) | Space: O(1)
+#         where n is the number of chars in string "t"
 def is_subsequence(s, t):
-	pass
+	#First case: If both s and t are the same string, then s is a substring of t
+	#Have two pointers. One to traverse s and the second to traverse t
+	#Traverse through t one char at a time.
+		#At each char, we want to check if the char matches the current char at s
+		#If no, we only move the t ptr to the next char
+		#If yes, we move both ptrs to their next char and then check to see if we already finished going through s
+			#If we reached end of s, we know that s is a substring of t
+	#If it reaches the end of t and we still haven't returned true, that means s is NOT a substring and we return False.
+
+	if s == t:
+		return True
+
+	ptrS = 0
+	for ptrT in range(len(t)):
+		if s[ptrS] != t[ptrT]:
+			ptrT += 1
+		else:
+			ptrT += 1
+			ptrS += 1
+			if ptrS == len(s):
+				return True
+	return False
+
+
 
 s1 = "abc"
 t1 = "ahbgdc"
