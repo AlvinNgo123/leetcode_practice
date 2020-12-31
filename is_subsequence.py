4a986332f3e8324d373695f96b9c2c79cab03dd2
Alvin Ngo <aln011@ucsd.edu>
2020-12-31 11:06:09 -0800

    Finished adding the testcases for is_subsequence code

---
+++ is_subsequence.py
@@ -0,0 +1,22 @@
+def is_subsequence(s, t):
+	pass
+
+s1 = "abc"
+t1 = "ahbgdc"
+print(is_subsequence(s1, t1))
+#True
+
+s2 = "axc"
+t2 = "ahbgdc"
+print(is_subsequence(s2, t2))
+#False
+
+s3 = "16-110"
+t3 = "5122256-1810"
+print(is_subsequence(s3, t3))
+#True
+
+s4 = ""
+t4 = ""
+print(is_subsequence(s4, t4))
+#True
