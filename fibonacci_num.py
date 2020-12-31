4d5698505b26771c7dfb807f7ea81c05e348c33d
Alvin Ngo <aln011@ucsd.edu>
2020-12-31 12:25:34 -0800

    Finished adding first soln for fib problem

--- fibonacci_num.py
+++ fibonacci_num.py
@@ -1,5 +1,18 @@
+#Runtime: O(n) | Space: O(n)
+#         where n is the input value
 def fibonacci_num(n):
-	pass
+	#Create an array of size n+1
+	#Set the two base cases for fib seq (0, 1)
+	#Iterate through array and fill out the rest of seq by summing the prior two values
+	#Return the last value in the array 
+
+	fibArr = [0 for x in range(n)]
+	fibArr[1] = 1
+
+	for i in range(2, len(fibArr)):
+		fibArr[i] = fibArr[i-1] + fibArr[i-2]
+
+	return fibArr[-1]
 
 n1 = 2
 print(fibonacci_num(n1))
@@ -11,4 +24,4 @@ print(fibonacci_num(n2))
 
 n3 = 6
 print(fibonacci_num(n3))
-#6
+#5
