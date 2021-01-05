f53f8dcb689584a1085bc773b8340fd948a9f3eb
Alvin Ngo <aln011@ucsd.edu>
2021-01-04 23:08:45 -0800

    Finished adding test cases for move_zeros code

---
+++ move_zeroes.py
@@ -0,0 +1,10 @@
+def move_zeroes(nums):
+	pass
+
+nums1 = [0, 1, 0, 0, 0, 3, 4, 0]
+print(move_zeroes(nums1))
+#[1, 3, 4, 2, 2, 2, 2, 2]
+
+nums2 = [0, 1, 0, 3, 12]
+print(move_zeroes(nums2))
+#[1, 3, 12, 0, 0]
