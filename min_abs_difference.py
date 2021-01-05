9aaac98b6e864b5e39c2c122eca78b4a9c85dded
Alvin Ngo <aln011@ucsd.edu>
2021-01-04 19:04:24 -0800

    Finished adding test cases for min abs diff

---
+++ min_abs_difference.py
@@ -0,0 +1,13 @@
+def min_abs_difference(arr1, arr2):
+	pass
+
+
+arr1_1 = [1, 6, 10, 15]
+arr1_2 = [3]
+print(min_abs_difference(arr1_1, arr1_2))
+#[1, 3]
+
+arr2_1 = [-1, 5, 10, 20, 28, 3]
+arr2_2 = [26, 134, 135, 15, 17]
+print(min_abs_difference(arr2_1, arr2_2))
+#[28, 26]
