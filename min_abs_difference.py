75fae6b4fca3579420f7501186770c13a680ab1d
Alvin Ngo <aln011@ucsd.edu>
2021-01-04 19:15:43 -0800

    Added brute force soln for min abs diff

--- min_abs_difference.py
+++ min_abs_difference.py
@@ -1,5 +1,23 @@
+#Runtime: O(nm) | Space: O(1)
+#         where n is the length of arr1 and m is the length of arr2
 def min_abs_difference(arr1, arr2):
-	pass
+	#loop though all possible pair combination and calculate the abs difference.
+	#keep track of the current minimum val (& corresponding pair) and update when a new min difference comes up.
+	#return the final min pair
+
+	currMinDiff = None
+	currMinPair = []
+
+	for i in range(len(arr1)):
+		for j in range(len(arr2)):
+			thisDiff = abs(arr1[i] - arr2[j])
+
+			if currMinDiff is None or currMinDiff > thisDiff:
+				currMinDiff = thisDiff
+				currMinPair = [arr1[i], arr2[j]]
+
+	return currMinPair
+
 
 
 arr1_1 = [1, 6, 10, 15]
