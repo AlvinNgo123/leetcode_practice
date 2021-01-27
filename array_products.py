804f850219ea7dbad5e47a238f0a63503a1a447f
Alvin Ngo <aln011@ucsd.edu>
2021-01-27 11:01:16 -0800

    Added final optimal soln for array products problem

--- array_products.py
+++ array_products.py
@@ -1,6 +1,33 @@
 #Runtime: O(n) | Space: O(n)
 #		  where n is length of array
 def array_products(array):
+    #We can make three additional arrays
+		#1) array with products of all values to the left of an index
+		#2) array with products of all values to the right of an index
+		#3) final array that multiplies the products of left and right of index
+	#We need to have a currentLeftProduct and currentRightProduct to keep track of our running product
+	#We first fill up the left (start from beginning), then we fill the right (start from end) 
+		#Each time, we use the current(Left or Right)Product to fill out curernt index of array 
+		#Then, we update the current(Left or Right)Product to include the current value
+	#Finally, go through final list and multiply the corersponding index's val in left and right to get final value
+    leftProducts = [0 for x in range(len(array))]
+    rightProducts = [0 for y in range(len(array))]
+    finalProducts = [0 for z in range(len(array))]
+    currLeftProd, currRightProd = 1, 1
+
+    for i in range(len(array)):
+        leftProducts[i] = currLeftProd
+        currLeftProd *= array[i]
+    for j in range(len(array)-1, -1, -1):
+        rightProducts[j] = currRightProd
+        currRightProd *= array[j]
+    for k in range(len(finalProducts)):
+        finalProducts[k] = leftProducts[k] * rightProducts[k]
+    return finalProducts
+
+#Runtime: O(n) | Space: O(n)
+#		  where n is length of array
+def array_products2(array):
     #This is a just a cleaner version of the other optimal soln
 	#However, we no longer need to have the left and right products array
 	#We can just modify the return array with our left products first
