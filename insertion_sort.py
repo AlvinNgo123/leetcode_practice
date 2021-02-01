78f0e2ffe032de621af405f32cc0b19c42a4a353
Alvin Ngo <aln011@ucsd.edu>
2021-02-01 14:08:23 -0800

    Finished adding code to insertion sort prob

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,18 @@
+#Average/Worse Runtime: O(n^2) | Space:O(1)
+#		  where n is the length of the array
+#Best Case Runtime: O(n)
 def insertion_sort(array):
-    pass
+    #Iterate through array and have the sorted list be on the left side of array and unsorted on the right side of array
+    #Keep swapping the current value back until it reaches a position where it's larger than the one on its left.
+    for i in range(1, len(array)):
+        currVal = i
+        while (array[currVal] < array[currVal-1]) and (currVal > 0):
+            temp = array[currVal]
+            array[currVal] = array[currVal-1]
+            array[currVal-1] = temp
+            currVal -= 1
+    return array
+        
 
 array1 = [3, 1, 2]
 print(insertion_sort(array1))
