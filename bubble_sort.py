8d1c38f20e0e2e22778c5f0518e34105953926be
Alvin Ngo <aln011@ucsd.edu>
2021-01-31 16:40:31 -0800

    Finished adding code for bubble sort problem

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,25 @@
+#Average/Worse Runtime: O(n^2) | Space:O(1)
+#		  where n is the length of the array
+#Best Case Runtime: O(n)
 def bubble_sort(array):
-    pass 
-
+    #Intialize a variable that keeps track of if a switch occurs during each pass to True
+    #Continue doing passes until variable remains True at end of pass
+        #reset swap var to False
+        #Comptre each curr element with its next and swap if curr is greater than next
+            #If swap occurs, change swap tracking variable to True
+        #If not, iterate to next element
+    #Return the array which is now sorted
+    swapOccurred = True
+    while swapOccurred == True:
+        swapOccurred = False
+        for i in range(len(array)-1):
+            if array[i] > array[i+1]:
+                temp = array[i+1]
+                array[i+1] = array[i]
+                array[i] = temp
+                swapOccurred = True
+    return array
+        
 array1 = [3, 1, 2]
 print(bubble_sort(array1))
 #[1, 2, 3]
