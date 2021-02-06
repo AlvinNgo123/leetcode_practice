a4bc1eea3ec6b53ae88741a82a6effed9b8e749d
Alvin Ngo <aln011@ucsd.edu>
2021-02-05 22:34:02 -0800

    Added test cases for selection sort problem

---
+++ selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(array):
+    pass
+
+array1 = [3, 1, 2]
+print(selection_sort(array1))
+#[1, 2, 3]
+
+array2 = [5, -2, 2, -8, 3, -10, -6, -1, 2, -2, 9, 1, 1]
+print(selection_sort(array2))
+#[-10, -8, -6, -2, -2, -1, 1, 1, 2, 2, 3, 5, 9]
+
+array3 = [31, 21, 12, -1, -2]
+print(selection_sort(array3))
+#[-2, -1, 12, 21, 31]
