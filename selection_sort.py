47a132c96a9fced2837ad091807fd5daf04af64f
Alvin Ngo <aln011@ucsd.edu>
2021-02-05 22:55:35 -0800

    Added code for selection sort problem

--- selection_sort.py
+++ selection_sort.py
@@ -1,8 +1,26 @@
+#Average/Worse Runtime: O(n^2) | Space:O(1)
+#		  where n is the length of the array
+#Best Case Runtime: O(n^2)
 def selection_sort(array):
-    pass
+    #Have a ptr that points to the first index of the unsorted list (this will be where swap occurs) i.e. swapPosition
+    #Have another ptr that iterates through all the values starting from that first ptr to the end of list and gets the index of its minimum value i.e checkPosition
+    #Perform the swap and then continue until the first ptr reaches the end of the list
+
+    for swapPosition in range(0, len(array)):
+        currMinVal = array[swapPosition]
+        currMinIdx = swapPosition
+        for checkPosition in range(swapPosition+1, len(array)):
+            if currMinVal > array[checkPosition]:
+                currMinVal = array[checkPosition]
+                currMinIdx = checkPosition
+        temp = array[swapPosition]
+        array[swapPosition] = currMinVal
+        array[currMinIdx] = temp
+    return array
+        
 
 array1 = [3, 1, 2]
-print(selection_sort(array1))
+print(selection_sort(array1)) 
 #[1, 2, 3]
 
 array2 = [5, -2, 2, -8, 3, -10, -6, -1, 2, -2, 9, 1, 1]
